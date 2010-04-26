0f78f217b9981131ece9efacd2e5bc2baaed742e
grzegorz herman <grzegorz.herman@gmail.com>
2010-04-26 23:46:17 +0200

    refactor (6/?): made satori a namespace package

--- satori/__init__.py
+++ satori/__init__.py
@@ -1,3 +1,6 @@
 # vim:ts=4:sts=4:sw=4:expandtab
-"""Package. The Satori project.
+"""Namespace package for the Satori project.
 """
+
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
