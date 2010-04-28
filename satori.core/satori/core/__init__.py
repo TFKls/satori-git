6e71daa34dcc9c342b8f95a00a53978b9116f11e
grzegorz herman <grzegorz.herman@gmail.com>
2010-04-28 23:57:47 +0200

    created entry point for contract generation

--- satori.core/satori/core/__init__.py
+++ satori.core/satori/core/__init__.py
@@ -2,3 +2,15 @@
 """The core of the system. Manages the database and operational logic. Functionality is
 exposed over Thrift.
 """
+
+
+def export_thrift():
+    """Entry Point. Writes the Thrift contract of the server to standard output.
+    """
+    from sys import stdout
+    import satori.core.models
+    from satori.ars import django2ars
+    from satori.ars.thrift import ThriftWriter
+    writer = ThriftWriter()
+    writer.contracts = django2ars.contracts
+    writer.writeTo(stdout)
