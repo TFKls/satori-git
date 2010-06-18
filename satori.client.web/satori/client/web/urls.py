c40a73a35d3304afca34ec98718258c0af78d221
duraj <duraj@tcs.uj.edu.pl>
2010-06-18 17:02:39 +0200

    Correction

--- satori.client.web/satori/client/web/urls.py
+++ satori.client.web/satori/client/web/urls.py
@@ -6,18 +6,18 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-#     (r'^$', 'satoritest.main.loaddefault'),
-	(r'^create/','satoritest.createdata.create'),
+#     (r'^$', 'satori.client.web.main.loaddefault'),
+	(r'^create/','satori.client.web.createdata.create'),
 	(r'^admin/', include(admin.site.urls)),
 	(r'^files/(?P<path>.*)/$', 'django.views.static.serve',
         {'document_root': 'files/'}),        
 	(r'^files/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': 'files/'}),
-	(r'^process/(?P<argstr>.*)/$','satoritest.main.loadPOST'),
-	(r'^process/(?P<argstr>.*)$','satoritest.main.loadPOST'),
-	(r'^(?P<argstr>.*)/(?P<path>.*)/$', 'satoritest.main.load'),
-    (r'^(?P<argstr>.*)/(?P<path>.*)$', 'satoritest.main.load'),
-    (r'^(?P<argstr>.*)$', 'satoritest.main.load')
+	(r'^process/(?P<argstr>.*)/$','satori.client.web.main.loadPOST'),
+	(r'^process/(?P<argstr>.*)$','satori.client.web.main.loadPOST'),
+	(r'^(?P<argstr>.*)/(?P<path>.*)/$', 'satori.client.web.main.load'),
+    (r'^(?P<argstr>.*)/(?P<path>.*)$', 'satori.client.web.main.load'),
+    (r'^(?P<argstr>.*)$', 'satori.client.web.main.load')
      
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
