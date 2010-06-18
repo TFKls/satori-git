99bd055d203b3206c5227504eefa13c7a55e859e
duraj <duraj@tcs.uj.edu.pl>
2010-06-18 17:25:36 +0200

    correct

--- satori.client.web/satori/client/web/settings.py
+++ satori.client.web/satori/client/web/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'satori.client.web.urls'
 
-TEMPLATE_DIRS = ("/afs/matinf.uj.edu.pl/usr/dokt/tcs/duraj/profil_TCS/satoritest/"
+TEMPLATE_DIRS = ("."
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
