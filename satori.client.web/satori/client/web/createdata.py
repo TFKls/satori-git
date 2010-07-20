f464176645c7737e7519f216ae3b2b6d9a5221cc
duraj <duraj@tcs.uj.edu.pl>
2010-07-20 17:42:05 +0200

    Changed createdata.py

--- satori.client.web/satori/client/web/createdata.py
+++ satori.client.web/satori/client/web/createdata.py
@@ -1,36 +1,36 @@
 from django.http import HttpResponse
-from satori.core.models import *
+from satori.client.common import classes
 
 def create(request):
-    ProblemMapping.objects.all().delete()
-    Problem.objects.all().delete()
-    Contestant.objects.all().delete()
-    Contest.objects.all().delete()
-    User.objects.all().delete()
-    MessageGlobal.objects.all().delete()
-    MessageContest.objects.all().delete()
-    paladin = User(fullname='Lech Duraj', login='paladin')
-    paladin.save()
-    User(fullname='Edgsger W. Dijkstra', login = 'dijkstra').save()
-    c2 = Contest(name = 'Kontest prywatny', joining = 'Private')
-    c3 = Contest(name = 'Kontest moderowany', joining = 'Moderated')
-    c4 = Contest(name = 'Kontest publiczny', joining = 'Public')
-    c2.save()
-    c3.save()
-    c4.save()
-    Contestant(user = paladin, contest = c2, accepted = True).save()
-    Contestant(user = paladin, contest = c3, accepted = True).save()
-    Contestant(user = paladin, contest = c4, accepted = True).save()
-    p1 = Problem(name = "SORT", description = "Zadanie o sortowaniu")
-    p2 = Problem(name = "COW", description = "Zadanie o krowie")
-    p3 = Problem(name = "WUWU", description = "Zadanie o wuwuzeli")
-    p1.save()
-    p2.save()
-    p3.save()
-    ProblemMapping(problem = p1, contest = c4, code = "A", title = "Harry Potter i sortownia smieci").save()
-    ProblemMapping(problem = p2, contest = c4, code = "B", title = "Harry Potter i krowa z Albanii").save()
-    ProblemMapping(problem = p3, contest = c4, code = "C", title = "Harry Potter i wuwuzele").save()
-    MessageGlobal(topic = "Wiadomosc pierwsza", content = "BZZZZZ!", mainscreenonly = True).save()
-    MessageGlobal(topic = "Wiadomosc systemowa", content = "Oglaszamy, ze za 5 minuBZZZZZ!", mainscreenonly = False).save()
-    MessageContest(topic = "Wiadomosc powitalna", content = "Publiczne BZZZZZ!", contest = c4).save()
+#    ProblemMapping.objects.all().delete()
+#    Problem.objects.all().delete()
+#    Contestant.objects.all().delete()
+#    Contest.objects.all().delete()
+#    User.objects.all().delete()
+#    MessageGlobal.objects.all().delete()
+#    MessageContest.objects.all().delete()
+    User = classes.User
+    Contest = classes.Contest
+    Contestant = classes.Contestant
+    Problem = classes.Problem
+    ProblemMapping = classes.ProblemMapping
+    MessageGlobal = classes.MessageGlobal
+    MessageContest = classes.MessageContest
+    paladin = User.create(fullname='Lech Duraj', login='paladin')
+    User.create(fullname='Edgsger W. Dijkstra', login = 'dijkstra')
+    c2 = Contest.create(name = 'Kontest prywatny', joining = 'Private')
+    c3 = Contest.create(name = 'Kontest moderowany', joining = 'Moderated')
+    c4 = Contest.create(name = 'Kontest publiczny', joining = 'Public')
+    Contestant.create(user = paladin, contest = c2, accepted = True)
+    Contestant.create(user = paladin, contest = c3, accepted = True)
+    Contestant.create(user = paladin, contest = c4, accepted = True)
+    p1 = Problem.create(name = "SORT", description = "Zadanie o sortowaniu")
+    p2 = Problem.create(name = "COW", description = "Zadanie o krowie")
+    p3 = Problem.create(name = "WUWU", description = "Zadanie o wuwuzeli")
+    ProblemMapping.create(problem = p1, contest = c4, code = "A", title = "Harry Potter i sortownia smieci")
+    ProblemMapping.create(problem = p2, contest = c4, code = "B", title = "Harry Potter i krowa z Albanii")
+    ProblemMapping.create(problem = p3, contest = c4, code = "C", title = "Harry Potter i wuwuzele")
+    MessageGlobal.create(topic = "Wiadomosc pierwsza", content = "BZZZZZ!", mainscreenonly = True)
+    MessageGlobal.create(topic = "Wiadomosc systemowa", content = "Oglaszamy, ze za 5 minuBZZZZZ!", mainscreenonly = False)
+    MessageContest.create(topic = "Wiadomosc powitalna", content = "Publiczne BZZZZZ!", contest = c4)
     return HttpResponse('OK!')
