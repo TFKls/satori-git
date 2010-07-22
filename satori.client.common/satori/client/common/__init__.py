3cfd958098330836603dfe46a31c0c8f3f3430ec
marek j wrobel <marek.j.wrobel@gmail.com>
2010-07-22 02:49:09 +0200

    Changed to use threaded client.

--- satori.client.common/satori/client/common/__init__.py
+++ satori.client.common/satori/client/common/__init__.py
@@ -1,39 +1,37 @@
 from thrift.transport.TSocket import TSocket
-from satori.ars.thrift import ThriftReader, ThriftClient
+from satori.ars.thrift import ThriftReader, ThriftClient, ThreadedThriftClient
 from satori.ars import ars2py
-from threading import local
+import sys
 
-class Classes(local):
-    def __init__(self, host, port):
-        self._port = port
-        self._host = host
+def transport_factory():
+    return TSocket(host='localhost', port=38889)
 
-        self._transport = None
-        self._client = None
-        self._classes = {}
+print 'Bootstrapping client...'
 
-    def __getattr__(self, name):
-        if self._client is None:
-            self._transport = TSocket(host=self._host, port=self._port)
-            self._client = ThriftClient(self._transport)
-            self._client.start(bootstrap=True)
-            self._classes = ars2py.process(self._client.contracts)
+_bootstrap_client = ThriftClient(transport_factory())
+_bootstrap_client.start(bootstrap=True)
+_bootstrap_client.stop()
 
-        if name in self._classes:
-            return self._classes[name]
-        else:
-            raise AttributeError('\'Classes\' object has no attribute \'{0}\''.format(name))
-       
-classes = Classes('localhost', 38889)
+_client = ThreadedThriftClient(_bootstrap_client.contracts, transport_factory)
+
+_module = sys.modules[__name__]
+
+_classes = ars2py.generate_classes(_client)
+
+for name, value in _classes.iteritems():
+	setattr(_module, name, value)
+
+setattr(_module, '__all__', _classes.keys())
+
+print 'Client bootstrapped.'
 
 def start_console():
     import code
     import readline
     console = code.InteractiveConsole()
-    console.runcode('import satori.client.common')
-    console.runcode('satori_classes = satori.client.common.classes')
+    console.runcode('import satori.client.common as satori_classes')
     print
-    print 'satori.client.common.classes is imported as satori_classes'
+    print 'satori.client.common is imported as satori_classes'
     print
     console.interact()
 
