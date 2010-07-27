3fbc6871b36462d8aaaf6c425a40af6fe914e314
marek j wrobel <marek.j.wrobel@gmail.com>
2010-07-27 15:58:32 +0200

    Added token support.

--- satori.client.common/satori/client/common/__init__.py
+++ satori.client.common/satori/client/common/__init__.py
@@ -5,26 +5,32 @@ import sys
 
 def transport_factory():
     return TSocket(host='satori.tcs.uj.edu.pl', port=38889)
-    
+
 print 'Bootstrapping client...'
-    
+
 _bootstrap_client = ThriftClient(transport_factory())
 _bootstrap_client.start(bootstrap=True)
 _bootstrap_client.stop()
-    
+
 _client = ThreadedThriftClient(_bootstrap_client.contracts, transport_factory)
-    
+
 _module = sys.modules[__name__]
-    
+
 _classes = ars2py.generate_classes(_client)
-    
+
 for name, value in _classes.iteritems():
     setattr(_module, name, value)
-        
-setattr(_module, '__all__', _classes.keys())
-        
+
+setattr(_module, '__all__', _classes.keys() + ['set_token', 'unset_token'])
+
 print 'Client bootstrapped.'
-        
+
+def set_token(token):
+    ars2py.token_container.set_token(token)
+
+def unset_token():
+    ars2py.token_container.unset_token()
+
 def start_console():
     import code
     import readline
@@ -34,5 +40,4 @@ def start_console():
     print 'satori.client.common is imported as satori_classes'
     print
     console.interact()
-                                        
-                                        
+
