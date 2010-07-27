85242fab37d12ba0353fc0feb2997aae3d997514
marek j wrobel <marek.j.wrobel@gmail.com>
2010-07-27 16:03:33 +0200

    Added performance counters support.

---
+++ satori.ars/satori/ars/perf.py
@@ -0,0 +1,23 @@
+import time
+
+_total = {}
+_begin = {}
+
+def begin(name):
+    _begin[name] = time.time()
+
+def end(name):
+    if not name in _begin:
+        raise Exception("Name not in _begin")
+
+    diff = time.time() - _begin[name]
+    del _begin[name]
+
+    _total[name] = _total.get(name, 0) + diff
+
+    print 'End:\t{0}\t{1}\t{2}'.format(name, diff, _total[name])
+
+def clear(name):
+    if name in _total:
+        del _total[name]
+
