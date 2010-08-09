cfcc138255c3a601e193e6f01af3625a54f13ee2
gzegzol <gzegzol@gmail.com>
2010-08-09 15:08:33 +0200

    Three is  a magic number.

--- satori.core/satori/core/api/Security.py
+++ satori.core/satori/core/api/Security.py
@@ -6,6 +6,7 @@ Security and authorization procedures.
 from datetime import datetime, timedelta
 from types import NoneType
 import string
+import random
 
 from satori.core.sec.tools import RightCheck, RoleSet, Token
 from satori.core.sec.store import Store
