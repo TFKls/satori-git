2ff033d7d6e0f1daf76952a111873a3fb0e8ac8d
gzegzol <gzegzol@gmail.com>
2010-08-23 19:37:26 +0200

    Handles pending notifications on startup.

--- satori.dbev/satori/dbev/notifier.py
+++ satori.dbev/satori/dbev/notifier.py
@@ -21,8 +21,91 @@ def row_to_dict(cursor, row):
             res[cursor.description[i][0]] = row[i]
     return res
 
+def handle_notifications(cursor):
+    while True:
+        cursor.execute('SELECT min(transaction) AS transaction FROM dbev_notification')
+        res = row_to_dict(cursor, cursor.fetchone())
+        if 'transaction' not in res:
+            break
+        if res['transaction'] == None:
+            break
+        transaction = int(res['transaction'])
+        cursor.execute('SELECT * FROM dbev_notification WHERE transaction=%s', [transaction])
+        events = {}
+        for row in cursor:
+            res = row_to_dict(cursor, row)
+            if 'object' not in res:
+                continue
+            id = int(res['object'])
+            table = str(res['table'])
+            if id not in events:
+                events[id] = {}
+            events[id][table] = res
+        print events
+        for id, tables in events.iteritems():
+            action = ''
+            user = None
+            previous = None
+            for table, data in tables.iteritems():
+                action = data['action']
+                if data['user'] != None:
+                    user = int(data['user'])
+                if data['previous'] != None:
+                    if previous == None or previous < int(data['previous']):
+                        previous = int(data['previous'])
+
+            if action == 'D':
+                ftrans = transaction-1
+            else:
+                ftrans = transaction
+
+            cursor.execute('SELECT model FROM ' + Object._meta.db_table + '__version_view(%s,%s)', [id, ftrans])
+            res = row_to_dict(cursor, cursor.fetchone())
+            if 'model' not in res:
+                continue
+            modelname = res['model']
+            model = models.get_model(*modelname.split('.'))
+            basemodel = model;
+            for table, data in tables.iteritems():
+                newmodel = models.get_model(*table.split('.'))
+                if issubclass(basemodel, newmodel):
+                    basemodel = newmodel
+
+            cursor.execute('SELECT model FROM ' + model._meta.db_table + '__version_view(%s,%s)', [id, ftrans])
+            rec = row_to_dict(cursor, cursor.fetchone())
+            while issubclass(model, basemodel):
+                event = Event(type='db')
+                event.object_id = id
+                event.transaction = transaction
+                event.action = action
+                event.user = user
+                event.previous = previous
+                event.model = model._meta.app_label + '.' + model._meta.module_name
+                if model in registry._registry:
+                    reg = registry._registry[model]
+                    if action == 'I' and reg.on_insert:
+                        for field in reg.on_insert:
+                            if field in rec:
+                                event['new.'+field] = rec[field]
+                        yield Send(event)
+                    if action == 'U' and reg.on_update:
+                        for field in reg.on_update:
+                            if field in rec:
+                                event['new.'+field] = rec[field]
+                        yield Send(event)
+                    if action == 'D' and reg.on_delete:
+                        for field in reg.on_delete:
+                            if field in rec:
+                                event['old.'+field] = rec[field]
+                        yield Send(event)
+                if len(model._meta.parents.items()) > 0:
+                    model = model._meta.parents.items()[0][0]
+                else:
+                    break
+        cursor.execute('DELETE FROM dbev_notification WHERE transaction=%s', [int(transaction)])
+
+
 def notifier_coroutine():
-    qn = connection.ops.quote_name
     while True:
         try:
             cursor = connection.cursor()
@@ -31,6 +114,8 @@ def notifier_coroutine():
             con.commit()
             cursor = con.cursor()
             cursor.execute('LISTEN satori;')
+            for action in handle_notifications(cursor):
+            	yield action
 
             while True:
                 if select.select([con], [], [], 5) == ([], [], []):
@@ -40,88 +125,9 @@ def notifier_coroutine():
                     if con.notifies:
                         while con.notifies:
                             con.notifies.pop()
-                        while True:
-                            cursor.execute('SELECT min(transaction) AS transaction FROM dbev_notification')
-                            res = row_to_dict(cursor, cursor.fetchone())
-                            if 'transaction' not in res:
-                            	break
-                            if res['transaction'] == None:
-                            	break
-                            transaction = int(res['transaction'])
-                            cursor.execute('SELECT * FROM dbev_notification WHERE transaction=%s', [transaction])
-                            events = {}
-                            for row in cursor:
-                                res = row_to_dict(cursor, row)
-                                if 'object' not in res:
-                                	continue
-                                id = int(res['object'])
-                                table = str(res['table'])
-                                if id not in events:
-                                    events[id] = {}
-                                events[id][table] = res
-                            print events
-                            for id, tables in events.iteritems():
-                                action = ''
-                                user = None
-                                previous = None
-                                for table, data in tables.iteritems():
-                                    action = data['action']
-                                    if data['user'] != None:
-                                        user = int(data['user'])
-                                    if data['previous'] != None:
-                                    	if previous == None or previous < int(data['previous']):
-                                        	previous = int(data['previous'])
-
-                                if action == 'D':
-                                	ftrans = transaction-1
-                                else:
-                                	ftrans = transaction
-
-                                cursor.execute('SELECT model FROM ' + Object._meta.db_table + '__version_view(%s,%s)', [id, ftrans])
-                                res = row_to_dict(cursor, cursor.fetchone())
-                                if 'model' not in res:
-                                	continue
-                                modelname = res['model']
-                                model = models.get_model(*modelname.split('.'))
-                                basemodel = model;
-                                for table, data in tables.iteritems():
-                                    newmodel = models.get_model(*table.split('.'))
-                                    if issubclass(basemodel, newmodel):
-                                        basemodel = newmodel
-
-                                cursor.execute('SELECT model FROM ' + model._meta.db_table + '__version_view(%s,%s)', [id, ftrans])
-                                rec = row_to_dict(cursor, cursor.fetchone())
-                                while issubclass(model, basemodel):
-                                    event = Event(type='db')
-                                    event.object = id
-                                    event.transaction = transaction
-                                    event.action = action
-                                    event.user = user
-                                    event.previous = previous
-                                    event.model = model._meta.app_label + '.' + model._meta.module_name
-                                    if model in registry._registry:
-                                        reg = registry._registry[model]
-                                        if action == 'I' and reg.on_insert:
-                                            for field in reg.on_insert:
-                                                if field in rec:
-                                                    event['new.'+field] = rec[field]
-                                            yield Send(event)
-                                        if action == 'U' and reg.on_update:
-                                            for field in reg.on_update:
-                                                if field in rec:
-                                                    event['new.'+field] = rec[field]
-                                            yield Send(event)
-                                        if action == 'D' and reg.on_delete:
-                                            for field in reg.on_delete:
-                                                if field in rec:
-                                                    event['old.'+field] = rec[field]
-                                            yield Send(event)
-                                    if len(model._meta.parents.items()) > 0:
-                                        model = model._meta.parents.items()[0][0]
-                                    else:
-                                        break
-                            cursor.execute('DELETE FROM dbev_notification WHERE transaction=%s', [int(transaction)])
+                        for action in handle_notifications(cursor):
+                            yield action
         except GeneratorExit:
-            raise
+            break
         except:
             traceback.print_exc()
