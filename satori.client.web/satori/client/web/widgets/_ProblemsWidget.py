f8b46bb0529c51f6196b599105999db9a4c3a0df
duraj <duraj@tcs.uj.edu.pl>
2010-09-07 18:07:23 +0200

    Added ProblemMapping creating by web interface.

--- satori.client.web/satori/client/web/widgets/_ProblemsWidget.py
+++ satori.client.web/satori/client/web/widgets/_ProblemsWidget.py
@@ -8,7 +8,12 @@ class ProblemsWidget(Widget):
     def __init__(self, params, path):
         self.htmlFile = 'htmls/problems.html'
         c = ActiveContest(params)
+        self.contest = c
+        self.manage = c.demand_right("MANAGE")
         self.problems = list()
         for p in ProblemMapping.filter({'contest':c}):
             editlink = GetLink(DefaultLayout(dict = params,maincontent = 'editprmap',problemid = [str(p.id)]),'')
             self.problems.append([p,editlink])
+        self.potential = Problem.filter()
+        self.back_to = ToString(params)
+        self.back_path = path
