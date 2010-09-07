5ccdcc11a6a96b960723543cace2414b7bc2c06e
duraj <duraj@tcs.uj.edu.pl>
2010-09-07 19:12:33 +0200

    nothing important here :)

--- satori.client.web/satori/client/web/widgets/_ProblemsWidget.py
+++ satori.client.web/satori/client/web/widgets/_ProblemsWidget.py
@@ -1,6 +1,7 @@
 ﻿from satori.client.web.URLDictionary import *
 from satori.client.web.queries import *
 from satori.client.common.remote import *
+from datetime import datetime
 from _Widget import Widget
 
 class ProblemsWidget(Widget):
@@ -8,12 +9,24 @@ class ProblemsWidget(Widget):
     def __init__(self, params, path):
         self.htmlFile = 'htmls/problems.html'
         c = ActiveContest(params)
+        r = c.contestant_role
         self.contest = c
         self.manage = c.demand_right("MANAGE")
         self.problems = list()
         for p in ProblemMapping.filter({'contest':c}):
             editlink = GetLink(DefaultLayout(dict = params,maincontent = 'editprmap',problemid = [str(p.id)]),'')
-            self.problems.append([p,editlink])
+            published = False
+            stime = None
+            ftime = None
+            privs = Privilege.filter({'role' : r, 'object' : p, 'right' : 'SUBMIT'})
+            for priv in privs:
+                if (not priv.startOn or priv.startOn>datetime.now()) and (not prov.finishOn or priv.endOn<datetime.now()):
+                    published = True
+                stime = str(priv.startOn)
+                ftime = str(priv.finishOn)
+            if published:
+                self.stime = 'Published'
+            self.problems.append([p,editlink,stime,ftime])
         self.potential = Problem.filter()
         self.back_to = ToString(params)
         self.back_path = path
