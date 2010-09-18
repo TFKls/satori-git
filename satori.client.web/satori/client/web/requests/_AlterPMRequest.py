1f60c819e963110a45b4ff5446ac1c48473aaf8f
duraj <duraj@tcs.uj.edu.pl>
2010-09-18 19:12:52 +0200

    Problem Mapping editing enabled.

---
+++ satori.client.web/satori/client/web/requests/_AlterPMRequest.py
@@ -0,0 +1,18 @@
+﻿# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from satori.client.common.remote import *
+from _Request import Request
+
+class AlterPMRequest(Request):
+    pathName = 'alterpm'
+    @classmethod
+    def process(cls, request):
+        pm = ProblemMapping.filter({'id':int(request.POST['pm_id'])})[0]
+        pm.statement = request.POST['statement']
+        pm.code = request.POST['code']
+        pm.title = request.POST['title']
+        d = ParseURL(request.POST['back_to'])
+        ret = DefaultLayout(dict=d,maincontent='problems')
+        return GetLink(ret,'')
