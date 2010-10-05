cc978e8e1b22f4b26ecad3963e31861081908fc0
marek j wrobel <marek.j.wrobel@gmail.com>
2010-10-06 00:10:44 +0200

    Fixed admin creation to work with Security.register() not returning the created user.

--- satori.core/satori/core/management.py
+++ satori.core/satori/core/management.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_syncdb
 
 def create_admin(app, created_models, verbosity, **kwargs):
     import satori.core.models
-    from satori.core.models import Entity
+    from satori.core.models import Entity, User, Privilege
 
     if (app != satori.core.models) or (Entity not in created_models):
         return
@@ -16,7 +16,8 @@ def create_admin(app, created_models, verbosity, **kwargs):
     print 'Creating superuser'
 
     token = Token('')
-    admin = ApiSecurity.Security_register.implementation(token, login=settings.ADMIN_NAME, fullname='Super Admin', password=settings.ADMIN_PASSWORD)
-    ApiPrivilege.Privilege_global_grant.implementation(token, admin, 'ADMIN')
+    ApiSecurity.Security_register.implementation(token, login=settings.ADMIN_NAME, fullname='Super Admin', password=settings.ADMIN_PASSWORD)
+    admin = User.objects.get(login='admin')
+    Privilege.global_grant(admin, 'ADMIN')
 
 post_syncdb.connect(create_admin)
