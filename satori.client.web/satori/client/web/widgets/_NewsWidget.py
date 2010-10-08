1eff2110a58857fe0187037276205e3737fd6b2f
kalq28 <kalq28@gmail.com>
2010-10-08 03:42:58 +0200

    Changed news rendering - from bbcode to rst.
    Also changed the look of Sign In and Register widgets.

--- satori.client.web/satori/client/web/widgets/_NewsWidget.py
+++ satori.client.web/satori/client/web/widgets/_NewsWidget.py
@@ -3,6 +3,8 @@ from satori.client.common.remote import *
 from satori.client.web.postmarkup import render_bbcode
 from _Widget import Widget
 
+from docutils.core import publish_parts
+
 # news table (a possible main content)
 class NewsWidget(Widget):
     pathName = 'news'
@@ -11,7 +13,7 @@ class NewsWidget(Widget):
         self.messages = []
         for m in MessageGlobal.filter():
             if not ActiveContest(params) or not m.mainscreenonly:
-                self.messages.append({'type' : 'global', 'topic' : m.topic, 'content' : render_bbcode(m.content)})
+                self.messages.append({'type' : 'global', 'topic' : m.topic, 'content' : publish_parts(m.content, writer_name='html')['fragment']})
         if (ActiveContest(params)):
             for m in MessageContest.filter({'contest':ActiveContest(params)}):
-                self.messages.append({'type' : 'contest', 'topic' : m.topic, 'content' : render_bbcode(m.content)})
+                self.messages.append({'type' : 'contest', 'topic' : m.topic, 'content' : publish_parts(m.content, writer_name='html')['fragment']})
