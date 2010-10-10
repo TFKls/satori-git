1c7e467807e1202cb83b8e5b153a766205af538b
duraj <duraj@tcs.uj.edu.pl>
2010-10-10 15:47:26 +0200

    Added subpage viewing (editing not available yet).

--- satori.client.web/satori/client/web/widgets/_ManageContestWidget.py
+++ satori.client.web/satori/client/web/widgets/_ManageContestWidget.py
@@ -1,6 +1,12 @@
 ﻿from _Widget import Widget
+from satori.client.web.queries import *
 
 class ManageContestWidget(Widget):
     pathName = 'mancontest'
     def __init__(self, params, path):
+        c = ActiveContest(params)
+        self.subpages = []
+        for s in Subpage.filter({'contest':c}):
+            editlink = ToString(DefaultLayout(dict=params,maincontent='editsubpage',subid=[str(s.id)]))
+            self.subpages.append([s,editlink])
         self.htmlFile = 'htmls/mancontest.html'
