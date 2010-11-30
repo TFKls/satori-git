ccc4b16089eb7333a5fb59d348adc7404a445e63
gzegzol <gzegzol@gmail.com>
2010-11-30 16:43:58 +0100

    CAS login

---
+++ satori.client.web/satori/client/web/requests/_CASFinishRequest.py
@@ -0,0 +1,21 @@
+﻿# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from satori.client.common.remote import *
+from _Request import Request
+
+class CASFinishRequest(Request):
+    pathName = 'cas_finish'
+    @classmethod
+    def process(cls, request):
+        vars = request.REQUEST
+        back_to = vars.get('back_to', '')
+        path = vars.get('path', '')
+        lw_path = vars.get('lw_path', '')
+        d = ParseURL(back_to)
+        try:
+            token_container.set_token(CentralAuthenticationService.finish(arg_map=dict(request.REQUEST.items()), return_to=request.build_absolute_uri()))
+        except:
+            follow(d,lw_path)['status'] = ['failed']
+        return GetLink(d,path)
---
+++ satori.client.web/satori/client/web/requests/_CASStartRequest.py
@@ -0,0 +1,40 @@
+﻿# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from satori.client.common.remote import *
+from _Request import Request
+from django.http import HttpResponse, HttpResponseRedirect
+
+import urlparse
+import urllib
+
+class CASStartRequest(Request):
+    pathName = 'cas_start'
+    @classmethod
+    def process(cls, request):
+        vars = request.REQUEST
+        d = ParseURL(vars.get('back_to', ''))
+        path = vars.get('path', '')
+        lw_path = vars['lw_path']
+        cas = vars['cas']
+        finisher = request.build_absolute_uri()
+        callback = urlparse.urlparse(finisher)
+        qs = urlparse.parse_qs(callback.query)
+        qs['back_to'] = (vars['back_to'],)
+        qs['path'] = (vars['path'],)
+        qs['lw_path'] = (vars['lw_path'],)
+        query = []
+        for key, vlist in qs.items():
+            for value in vlist:
+                query.append((key,value))
+        query = urllib.urlencode(query)
+        path = '/process.cas_finish'
+        finisher = urlparse.urlunparse((callback.scheme, callback.netloc, path, callback.params, query, callback.fragment))
+        try:
+            res = CentralAuthenticationService.start(realm_name=cas, return_to=finisher)
+            token_container.set_token(res['token'])
+            return HttpResponseRedirect(res['redirect'])
+        except:
+            follow(d,lw_path)['status'] = ['failed']
+        return GetLink(d, path)
