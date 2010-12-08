f0ed76bb29828dd0d0940f7602cd8be2740d3edf
marek j wrobel <marek.j.wrobel@gmail.com>
2010-12-08 01:10:38 +0100

    OA methods have 'Attribute group: name' in docstrings.

--- satori.core/satori/core/export/oa.py
+++ satori.core/satori/core/export/oa.py
@@ -37,6 +37,7 @@ def fixup_{1}(self):
 code_attributegroup_methods = """
 @ExportMethod(Attribute, [DjangoId('{0}'), unicode], PCArg('self', '{3}'))
 def {1}_get(self, name):
+    \"\"\"Attribute group: {1}\"\"\"
     self = self{2}
     try:
         return self.attributes.get(name=name)
@@ -45,6 +46,7 @@ def {1}_get(self, name):
 
 @ExportMethod(unicode, [DjangoId('{0}'), unicode], PCArg('self', '{3}'))
 def {1}_get_str(self, name):
+    \"\"\"Attribute group: {1}\"\"\"
     oa = self.{1}_get(name)
     if oa is None:
         return None
@@ -55,6 +57,7 @@ def {1}_get_str(self, name):
 
 @ExportMethod(unicode, [DjangoId('{0}'), unicode], PCArg('self', '{3}'))
 def {1}_get_blob(self, name):
+    \"\"\"Attribute group: {1}\"\"\"
     oa = self.{1}_get(name)
     if oa is None:
         return None
@@ -64,6 +67,7 @@ def {1}_get_blob(self, name):
 
 @ExportMethod(unicode, [DjangoId('{0}'), unicode], PCArg('self', '{3}'))
 def {1}_get_blob_hash(self, name):
+    \"\"\"Attribute group: {1}\"\"\"
     oa = self.{1}_get(name)
     if oa is None:
         return None
@@ -74,14 +78,17 @@ def {1}_get_blob_hash(self, name):
 
 @ExportMethod(TypedList(Attribute), [DjangoId('{0}')], PCArg('self', '{3}'))
 def {1}_get_list(self):
+    \"\"\"Attribute group: {1}\"\"\"
     return self{2}.attributes.all()
 
 @ExportMethod(TypedMap(unicode, AnonymousAttribute), [DjangoId('{0}')], PCArg('self', '{3}'))
 def {1}_get_map(self):
+    \"\"\"Attribute group: {1}\"\"\"
     return dict((oa.name, oa) for oa in self{2}.attributes.all())
 
 @ExportMethod(NoneType, [DjangoId('{0}'), Attribute], PCAnd(PCArg('self', '{4}'), PCRawBlob('value')))
 def {1}_set(self, value):
+    \"\"\"Attribute group: {1}\"\"\"
     self = self{2}
     (newoa, created) = self.attributes.get_or_create(name=value.name)
     newoa.is_blob = value.is_blob
@@ -94,41 +101,49 @@ def {1}_set(self, value):
 
 @ExportMethod(NoneType, [DjangoId('{0}'), unicode, unicode], PCArg('self', '{4}'))
 def {1}_set_str(self, name, value):
+    \"\"\"Attribute group: {1}\"\"\"
     self.{1}_set(Attribute(name=name, value=value, is_blob=False))
 
 @ExportMethod(NoneType, [DjangoId('{0}'), unicode, int, unicode], PCArg('self', '{4}'))
 def {1}_set_blob(self, name, length=-1, filename=''):
+    \"\"\"Attribute group: {1}\"\"\"
     def set_hash(hash):
         self.{1}_set(OpenAttribute(name=name, value=hash, filename=filename, is_blob=True))
     return Blob.create(length, set_hash)
 
 @ExportMethod(NoneType, [DjangoId('{0}'), unicode, unicode, unicode], PCAnd(PCArg('self', '{4}'), PCGlobal('RAW_BLOB')))
 def {1}_set_blob_hash(self, name, value, filename=''):
+    \"\"\"Attribute group: {1}\"\"\"
     self.{1}_set(Attribute(name=name, value=value, filename=filename, is_blob=True))
 
 @ExportMethod(NoneType, [DjangoId('{0}'), TypedList(Attribute)], PCAnd(PCArg('self', '{4}'), PCEach('attributes', PCRawBlob('item'))))
 def {1}_add_list(self, attributes):
+    \"\"\"Attribute group: {1}\"\"\"
     for struct in attributes:
         self.{1}_set(struct)
 
 @ExportMethod(NoneType, [DjangoId('{0}'), TypedList(Attribute)], PCAnd(PCArg('self', '{4}'), PCEach('attributes', PCRawBlob('item'))))
 def {1}_set_list(self, attributes):
+    \"\"\"Attribute group: {1}\"\"\"
     self{2}.attributes.all().delete()
     self.{1}_add_list(attributes)
 
 @ExportMethod(NoneType, [DjangoId('{0}'), TypedMap(unicode, AnonymousAttribute)], PCAnd(PCArg('self', '{4}'), PCEachValue('attributes', PCRawBlob('item'))))
 def {1}_add_map(self, attributes):
+    \"\"\"Attribute group: {1}\"\"\"
     for name, struct in attributes.items():
         struct.name = name
         self.{1}_set(struct)
 
 @ExportMethod(NoneType, [DjangoId('{0}'), TypedMap(unicode, AnonymousAttribute)], PCAnd(PCArg('self', '{4}'), PCEachValue('attributes', PCRawBlob('item'))))
 def {1}_set_map(self, attributes):
+    \"\"\"Attribute group: {1}\"\"\"
     self{2}.attributes.all().delete()
     self.{1}_add_map(attributes)
 
 @ExportMethod(NoneType, [DjangoId('{0}'), unicode], PCArg('self', '{4}'))
 def {1}_delete(self, name):
+    \"\"\"Attribute group: {1}\"\"\"
     oa = self.{1}_get(name)
     if oa is not None:
         oa.delete()
