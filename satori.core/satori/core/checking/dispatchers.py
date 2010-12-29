0c9df5e48e25ac87df624cd10cb54d1155b2279a
gzegzol <gzegzol@gmail.com>
2010-12-29 17:45:23 +0100

    New dispatchers.

--- satori.core/satori/core/checking/dispatchers.py
+++ satori.core/satori/core/checking/dispatchers.py
@@ -1,21 +1,26 @@
 # vim:ts=4:sts=4:sw=4:expandtab
 import logging
-from django.db import transaction
 from collections import deque
 
 from satori.core.checking.accumulators import accumulators
-from satori.core.checking.utils import wrap_transaction_management
-from satori.core.models import Test, TestResult
-from satori.events import Event, Client2
+from satori.core.models import Test
 
-class DispatcherBase(Client2):
-    def __init__(self, test_suite_result, accumulator_list, runner): 
+class DispatcherBase(object):
+    def __init__(self, supervisor, test_suite_result, accumulator_list = []):
         super(DispatcherBase, self).__init__()
+        self.supervisor = supervisor
         self.test_suite_result = test_suite_result
-        self.runner = runner
         self.accumulators = [
-                accumulators[accumulator](test_suite_result) for accumulator in accumulator_list
+            accumulators[accumulator](test_suite_result) for accumulator in accumulator_list
         ]
+        for accumulator in self.accumulators:
+            accumulator.init()
+    def checked_test_result(self, test_results):
+        raise NotImplementedError
+    def rejudge_test_result(self, test_results):
+        raise NotImplementedError
+    def changed_overrides(self):
+        raise NotImplementedError
 
     def accumulate(self, test_result):
         for accumulator in self.accumulators:
@@ -24,152 +29,57 @@ class DispatcherBase(Client2):
     def status(self):
         return all(accumulator.status() for accumulator in self.accumulators)
 
-    def do_init(self):
-        self.error = False
-        self.queue = 'dispatcher_{0}'.format(self.test_suite_result.id)
-        self.attach(self.queue)
-        self.map({'type': 'db', 'model': 'core.testresult', 'action': 'U', 'new.pending': False, 'new.submit': self.test_suite_result.submit.id}, self.queue)
-
-        for accumulator in self.accumulators:
-            accumulator.init()
-
-    def do_deinit(self):
+    def finish(self):
         for accumulator in self.accumulators:
             accumulator.deinit()
-
-        self.test_suite_result.pending = False
-        self.test_suite_result.save()
-
-    def do_handle_event(self, queue, event):
-        pass
-    
-    @wrap_transaction_management
-    def init(self):
-        try:
-            self.do_init()
-            transaction.commit()
-        except:
-            logging.exception('Dispatcher error')
-            self.error = True
-            self.finish()
-            transaction.rollback()
-
-    @wrap_transaction_management
-    def deinit(self):
-        if not self.error:
-            try:
-                self.do_deinit()
-                transaction.commit()
-            except:
-                logging.exception('Dispatcher error')
-                self.error = True
-                transaction.rollback()
-        if self.error:
-            try:
-                self.test_suite_result.oa_set_str('status', 'INT')
-                self.test_suite_result.status = 'INT'
-                self.test_suite_result.report = 'Internal error'
-                self.test_suite_result.pending = False
-                self.test_suite_result.save()
-                transaction.commit()
-            except:
-                logging.exception('Dispatcher error handler error')
-                transaction.rollback()
-        self.runner.dispatcher_stopped(self.test_suite_result)
-
-    @wrap_transaction_management
-    def handle_event(self, queue, event):
-        try:
-            self.do_handle_event(queue, event)
-            transaction.commit()
-        except:
-            logging.exception('Dispatcher error')
-            self.error = True
-            self.finish()
-            transaction.rollback()
-
+        self.supervisor.finished_test_suite_result()
 
 class SerialDispatcher(DispatcherBase):
     """Serial dispatcher"""
 
-    def __init__(self, test_suite_result, accumulator_list, runner):
-        super(SerialDispatcher, self).__init__(test_suite_result, accumulator_list, runner)
-
-    def send_test(self):
-        while self.to_check:
-            next_test = Test.objects.get(id=self.to_check.popleft())
-            (next_test_result, created) = TestResult.objects.get_or_create(submit=self.test_suite_result.submit, test=next_test)
-            if next_test_result.pending:
-                self.next_test_result_id = next_test_result.id
-                return
-            else:
-                self.accumulate(next_test_result)
-                if not self.status():
-                    self.finish()
-                    return
-
-        self.finish()
-
-    def do_init(self):
-        super(SerialDispatcher, self).do_init()
-
-        self.next_test_result_id = -1
-
+    def __init__(self, supervisor, test_suite_result, accumulator_list = []):
+        super(SerialDispatcher, self).__init__(supervisor, test_suite_result, accumulator_list)
         self.to_check = deque()
         for test in self.test_suite_result.test_suite.tests.all():
             self.to_check.append(test.id)
-
         self.send_test()
 
-    def do_deinit(self):
-        super(SerialDispatcher, self).do_deinit()
+    def checked_test_result(self, test_results):
+        for result in test_results:
+            assert result.id == self.to_check[0]
+            self.to_check.popleft()
+            self.accumulate(result)
+            if not self.status():
+                self.finish()
+        self.send_test()
 
-    def do_handle_event(self, queue, event):
-        if event.object_id != self.next_test_result_id:
-            return
-
-        test_result = TestResult.objects.get(id=event.object_id)
-        self.accumulate(test_result)
-
-        if not self.status():
-            self.finish()
+    def send_test(self):
+        if self.to_check:
+            submit = test_suite_result.submit
+            test = Test.objects.get(id=self.to_check[0])
+            self.supervisor.schedule_test_result(test_suite_result=self.test_suite_result, submit=submit, test=test)
         else:
-            self.send_test()
-
+            self.finish()
 
 class ParallelDispatcher(DispatcherBase):
     """Parallel dispatcher"""
 
-    def __init__(self, test_suite_result, accumulator_list, runner):
-        super(ParallelDispatcher, self).__init__(test_suite_result, accumulator_list, runner)
-
-    def do_init(self):
-        super(ParallelDispatcher, self).do_init()
-
-        self.wanted_test_result_ids = set()
+    def __init__(self, supervisor, test_suite_result, accumulator_list = []):
+        super(ParallelDispatcher, self).__init__(supervisor, test_suite_result, accumulator_list)
+        submit = test_suite_result.submit
+        self.to_check = set()
         for test in self.test_suite_result.test_suite.tests.all():
-            (test_result, created) = TestResult.objects.get_or_create(submit=self.test_suite_result.submit, test=test)
-            if test_result.pending:
-                self.wanted_test_result_ids.add(test_result.id)
-            else:
-                self.accumulate(test_result)
+            self.to_check.add(test.id)
+            self.supervisor.schedule_test_result(test_suite_result=self.test_suite_result, submit=submit, test=test)
 
-        if not self.wanted_test_result_ids:
-            self.finish()
-
-    def do_deinit(self):
-        super(ParallelDispatcher, self).do_deinit()
-
-    def do_handle_event(self, queue, event):
-        if event.object_id not in self.wanted_test_result_ids:
-            return
-
-        self.wanted_test_result_ids.remove(event.object_id)
-
-        test_result = TestResult.objects.get(id=event.object_id)
-        self.accumulate(test_result)
-
-        if not self.wanted_test_result_ids:
+    def checked_test_result(self, test_results):
+        for result in test_results:
+            assert result.id in self.to_check
+            self.to_check.remove(result.id)
+            self.accumulate(result)
+            if not self.status():
+                self.finish()
+        if not self.to_check:
             self.finish()
 
 dispatchers = {}
