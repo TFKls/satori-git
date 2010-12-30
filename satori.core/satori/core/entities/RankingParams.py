6660636e650d7db484327e25be4ee2c26abaeffd
gzegzol <gzegzol@gmail.com>
2010-12-30 15:43:28 +0100

    RankingParams.

---
+++ satori.core/satori/core/entities/RankingParams.py
@@ -0,0 +1,37 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+
+from satori.core.dbev               import Events
+
+from satori.core.models import Entity
+
+@ExportModel
+class RankingParams(Entity):
+    """Model. Intermediary for many-to-many relationship between Rankings and
+    ProblemMappings.
+    """
+
+    parent_entity = models.OneToOneField(Entity, parent_link=True, related_name='cast_rankingparams')
+
+    ranking     = models.ForeignKey('Ranking')
+    problem     = models.ForeignKey('ProblemMapping')
+    test_suite  = models.ForeignKey('TestSuite', null=True)
+
+    class ExportMeta(object):
+        fields = [('ranking', 'VIEW'), ('problem', 'VIEW'), ('test_suite', 'VIEW')]
+
+    @classmethod
+    def inherit_rights(cls):
+        inherits = super(RankingParams, cls).inherit_rights()
+        cls._inherit_add(inherits, 'MANAGE', 'ranking', 'MANAGE')
+        return inherits
+
+    class Meta:                                                # pylint: disable-msg=C0111
+        unique_together = (('ranking', 'problem'),)
+
+class RankingParamsEvents(Events):
+    model = RankingParams
+    on_insert = on_update = ['ranking', 'problem', 'test_suite']
+    on_delete = []
+
