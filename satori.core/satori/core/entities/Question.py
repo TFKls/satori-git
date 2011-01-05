14d15255b8b224823141bbab979e497b84d3c252
marek j wrobel <marek.j.wrobel@gmail.com>
2011-01-05 23:23:51 +0100

    Changed Message to Question, removed additional Message types.

---
+++ satori.core/satori/core/entities/Question.py
@@ -0,0 +1,25 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+
+from satori.core.dbev   import Events
+from satori.core.models import Entity
+
+@ExportModel
+class Question(Entity):
+    """Model. Description of a question.
+    """
+    parent_entity = models.OneToOneField(Entity, parent_link=True, related_name='cast_question')
+
+    problem       = models.ForeignKey('ProblemMapping', null=True)
+    contest       = models.ForeignKey('Contest')
+    content       = models.TextField(blank=True)
+    time          = models.DateTimeField(auto_now_add=True)
+
+    class ExportMeta(object):
+        fields = [('problem', 'VIEW'), ('contest', 'VIEW'), ('content', 'VIEW'), ('time', 'VIEW')]
+
+class QuestionEvents(Events):
+    model = Question
+    on_insert = on_update = ['topic', 'time']
+    on_delete = []
