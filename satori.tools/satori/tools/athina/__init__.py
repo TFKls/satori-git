f659357695522a267391fcb3cc15a98b7fe7d04d
gzegzol <gzegzol@gmail.com>
2011-01-08 17:35:57 +0100

    SerialDispatcher in athina importer.

--- satori.tools/satori/tools/athina/__init__.py
+++ satori.tools/satori/tools/athina/__init__.py
@@ -219,7 +219,7 @@ def athina_import():
                 oa.set_str('time', str(10*int(test['timelimit'])))
             test['object'] = Creator('Test', problem=problem['object'], name=options.name + '_' + problem['problem'] + '_default_' + str(test['test'])).fields(environment=options.environment).additional(data=oa.get_map())()
             tests.append(test['object'])
-        problem['testsuite'] = Creator('TestSuite', problem=problem['object'], name=options.name + '_' + problem['problem'] + '_default').fields(dispatcher='ParallelDispatcher', accumulators='StatusAccumulator').additional(test_list=tests)()
+        problem['testsuite'] = Creator('TestSuite', problem=problem['object'], name=options.name + '_' + problem['problem'] + '_default').fields(dispatcher='SerialDispatcher', accumulators='StatusAccumulator').additional(test_list=tests)()
         problem['mapping'] = Creator('ProblemMapping', contest=contest, problem=problem['object']).fields(code=problem['problem'], title=problem['problem'], default_test_suite=problem['testsuite'])()
         Creator('Ranking', contest=contest, name='Ranking').fields(is_public=True, aggregator='CountAggregator')()
         Creator('Ranking', contest=contest, name='Full Ranking').fields(is_public=False, aggregator='CountAggregator')()
