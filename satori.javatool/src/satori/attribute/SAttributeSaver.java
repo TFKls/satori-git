7576f0085552b6f551f9226089a6e2fe6b935888
bartosz m walczak <bartosz.m.walczak@gmail.com>
2011-02-02 10:34:43 +0100

    removed unnecessary class

--- satori.javatool/src/satori/attribute/SAttributeSaver.java
+++
@@ -1,8 +0,0 @@
-package satori.attribute;
-
-import satori.blob.SBlob;
-
-public interface SAttributeSaver {
-	void saveString(String name, String value);
-	void saveFile(String name, SBlob blob);
-}
