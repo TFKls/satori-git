f65af5b11a33346249e7173bc7a5fbcd21415e27
bartosz m walczak <bartosz.m.walczak@gmail.com>
2011-02-03 23:48:44 +0100

    test drop indication

--- satori.javatool/src/satori/test/ui/STestPane.java
+++ satori.javatool/src/satori/test/ui/STestPane.java
@@ -1,17 +1,25 @@
 package satori.test.ui;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TooManyListenersException;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -22,8 +30,8 @@ import javax.swing.TransferHandler;
 
 import satori.common.SException;
 import satori.common.SList;
-import satori.common.SListener1;
 import satori.common.SListener0;
+import satori.common.SListener1;
 import satori.common.SListener2;
 import satori.common.ui.SPane;
 import satori.common.ui.SScrollPane;
@@ -64,6 +72,31 @@ public class STestPane implements SList<STestImpl>, SPane {
 		@Override public void call(SSolutionPane removed_pane) { removeSolutionPane(removed_pane); }
 	};
 	
+	private int indicator_index = -1;
+	private Rectangle indicator_rect = null;
+	private boolean accept_drop = false;
+	
+	private int getDropIndex(Point location) {
+		int index = (int)Math.round((location.getX() - SDimension.labelWidth) / SDimension.itemWidth);
+		if (index < 0) index = 0;
+		if (index > tests.size()) index = tests.size();
+		return index;
+	}
+	private void indicateDrop(Point location) {
+		int index = location != null && accept_drop ? getDropIndex(location) : -1;
+		if (index == indicator_index) return;
+		indicator_index = index;
+		if (indicator_rect != null) {
+			input_pane.getPane().repaint(indicator_rect);
+			indicator_rect = null;
+		}
+		if (index == -1) return;
+		int x = SDimension.labelWidth + index * SDimension.itemWidth - 1;
+		int height = input_pane.getPane().getHeight();
+		indicator_rect = new Rectangle(x, 0, 1, height);
+		input_pane.getPane().repaint(indicator_rect);
+	}
+	
 	private static class MoveTestTransferable implements Transferable {
 		private final STestTransfer data;
 		public MoveTestTransferable(STestTransfer data) { this.data = data; }
@@ -85,21 +118,31 @@ public class STestPane implements SList<STestImpl>, SPane {
 		private STestImpl test;
 		
 		@Override public boolean canImport(TransferSupport support) {
-			if ((support.getSourceDropActions() & COPY) != COPY) return false;
-			support.setDropAction(COPY);
-			if (support.isDataFlavorSupported(STestSnapTransfer.flavor)) return true;
+			if (!support.isDrop()) return false;
+			accept_drop = false;
+			if (support.isDataFlavorSupported(STestSnapTransfer.flavor)) {
+				if ((support.getSourceDropActions() & COPY) != COPY) return false;
+				support.setDropAction(COPY);
+				accept_drop = true;
+				return true;
+			}
 			if (support.isDataFlavorSupported(STestTransfer.flavor)) {
+				if ((support.getSourceDropActions() & MOVE) != MOVE) return false;
+				support.setDropAction(MOVE);
 				Transferable t = support.getTransferable();
 				STestTransfer data;
 				try { data = (STestTransfer)t.getTransferData(STestTransfer.flavor); }
 				catch(UnsupportedFlavorException ex) { return false; }
 				catch(IOException ex) { return false; }
-				return data.getTestSuite() == suite;
+				if (data.getTestSuite() != suite) return false;
+				accept_drop = true;
+				return true;
 			}
 			return false;
 		}
 		@Override public boolean importData(TransferSupport support) {
 			if (!support.isDrop()) return false;
+			accept_drop = false;
 			Transferable t = support.getTransferable();
 			if (support.isDataFlavorSupported(STestSnapTransfer.flavor)) {
 				STestSnapTransfer data;
@@ -112,43 +155,45 @@ public class STestPane implements SList<STestImpl>, SPane {
 					try { new_tests.add(factory.create(snap)); }
 					catch(SException ex) { SFrame.showErrorDialog(ex); return false; }
 				}
-				Point pos = support.getDropLocation().getDropPoint();
-				int index = (int)Math.round((pos.getX() - SDimension.labelWidth) / SDimension.itemWidth);
-				if (index < 0) index = 0;
-				if (index > tests.size()) index = tests.size();
+				int index = getDropIndex(support.getDropLocation().getDropPoint());
 				int cur_index = index;
 				for (STestImpl test : new_tests) suite.addTest(test, cur_index++);
 				add(new_tests, index);
 				return true;
 			}
-			else if (support.isDataFlavorSupported(STestTransfer.flavor)) {
+			if (support.isDataFlavorSupported(STestTransfer.flavor)) {
 				STestTransfer data;
 				try { data = (STestTransfer)t.getTransferData(STestTransfer.flavor); }
 				catch(UnsupportedFlavorException ex) { return false; }
 				catch(IOException ex) { return false; }
 				if (data.getTestSuite() != suite) return false;
 				STestImpl test = data.getTest();
-				Point pos = support.getDropLocation().getDropPoint();
-				int index = (int)Math.round((pos.getX() - SDimension.labelWidth) / SDimension.itemWidth);
-				if (index < 0) index = 0;
-				if (index > tests.size()) index = tests.size();
+				int index = getDropIndex(support.getDropLocation().getDropPoint());
 				suite.moveTest(test, index);
 				move(test, index);
 				return true;
 			}
-			else return false;
+			return false;
 		}
 		@Override protected Transferable createTransferable(JComponent c) {
 			return new MoveTestTransferable(new STestTransfer(test, suite));
 		}
-		@Override public int getSourceActions(JComponent c) { return COPY; }
+		@Override public int getSourceActions(JComponent c) { return MOVE; }
 		@Override protected void exportDone(JComponent source, Transferable data, int action) {}
 		@Override public void call(STestImpl test, MouseEvent e) {
 			this.test = test;
-			exportAsDrag(scroll_pane.getPane(), e, TransferHandler.COPY);
+			exportAsDrag(scroll_pane.getPane(), e, TransferHandler.MOVE);
 		}
 	}
 	private TestTransferHandler transfer_handler = new TestTransferHandler();
+	
+	private DropTargetListener drop_listener = new DropTargetListener() {
+		@Override public void dragEnter(DropTargetDragEvent e) { indicateDrop(e.getLocation()); }
+		@Override public void dragOver(DropTargetDragEvent e) { indicateDrop(e.getLocation()); }
+		@Override public void dragExit(DropTargetEvent e) { indicateDrop(null); }
+		@Override public void drop(DropTargetDropEvent e) { indicateDrop(null); }
+		@Override public void dropActionChanged(DropTargetDragEvent e) { indicateDrop(e.getLocation()); }
+	};
 	
 	public STestPane(STestMetadata meta, STestSuiteImpl suite, STestFactory factory) {
 		this.meta = meta;
@@ -159,8 +204,19 @@ public class STestPane implements SList<STestImpl>, SPane {
 	
 	@Override public JComponent getPane() { return scroll_pane.getPane(); }
 	
+	@SuppressWarnings("serial")
+	private class Pane extends Box {
+		public Pane() { super(BoxLayout.Y_AXIS); }
+		@Override public void paint(Graphics g) {
+			super.paint(g);
+			if (indicator_rect == null) return;
+			g.setColor(Color.BLUE);
+			g.fillRect(indicator_rect.x, indicator_rect.y, indicator_rect.width, indicator_rect.height);
+		}
+	}
+	
 	private void initialize() {
-		pane = new Box(BoxLayout.Y_AXIS);
+		pane = new Pane();
 		input_pane = new STestInputPane();
 		input_pane.addRow(new SStatusRowView());
 		input_pane.addRow(new SButtonRowView(transfer_handler, close_test_listener, new_test_listener));
@@ -180,6 +236,8 @@ public class STestPane implements SList<STestImpl>, SPane {
 		scroll_pane = new SScrollPane();
 		scroll_pane.setView(pane);
 		scroll_pane.getPane().setTransferHandler(transfer_handler);
+		try { scroll_pane.getPane().getDropTarget().addDropTargetListener(drop_listener); }
+		catch(TooManyListenersException ex) {}
 	}
 	
 	private void addSolutionPane() {
