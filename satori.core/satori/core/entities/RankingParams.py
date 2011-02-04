c4010c4fb19156dcdb8e696459c053d0ce148fbd
marek j wrobel <marek.j.wrobel@gmail.com>
2011-02-04 17:46:04 +0100

    Added parameters in Ranking. RankingParams are not exported.

--- satori.core/satori/core/entities/RankingParams.py
+++ satori.core/satori/core/entities/RankingParams.py
@@ -5,22 +5,28 @@ from django.db import models
 from satori.core.dbev   import Events
 from satori.core.models import Entity
 
-@ExportModel
+#@ExportModel
 class RankingParams(Entity):
     """Model. Intermediary for many-to-many relationship between Rankings and
     ProblemMappings.
     """
     parent_entity = models.OneToOneField(Entity, parent_link=True, related_name='cast_rankingparams')
 
-    ranking       = models.ForeignKey('Ranking', related_name='ranking_params+')
+    ranking       = models.ForeignKey('Ranking', related_name='ranking_params')
     problem       = models.ForeignKey('ProblemMapping', related_name='ranking_params+')
     test_suite    = models.ForeignKey('TestSuite', related_name='ranking_params+', null=True)
+
+    params        = AttributeGroupField(PCArg('self', 'MANAGE'), PCDeny(), '')
 
     class Meta:                                                # pylint: disable-msg=C0111
         unique_together = (('ranking', 'problem'),)
 
-    class ExportMeta(object):
-        fields = [('ranking', 'VIEW'), ('problem', 'VIEW'), ('test_suite', 'VIEW')]
+#    class ExportMeta(object):
+#        fields = [('ranking', 'VIEW'), ('problem', 'VIEW'), ('test_suite', 'VIEW')]
+
+    def save(*args, **kwargs):
+        self.fixup_params()
+        super(RankingParams, self).save(*args, **kwargs)
 
     @classmethod
     def inherit_rights(cls):
@@ -28,27 +34,27 @@ class RankingParams(Entity):
         cls._inherit_add(inherits, 'MANAGE', 'ranking', 'MANAGE')
         return inherits
 
-    @ExportMethod(DjangoStruct('RankingParams'), [DjangoStruct('RankingParams')], PCArgField('fields', 'ranking', 'MANAGE'), [CannotSetField])
-    @staticmethod
-    def create(fields):
-        params = RankingParams()
-        params.forbid_fields(fields, ['id'])
-        params.update_fields(fields, ['ranking', 'problem', 'test_suite'])
-        if params.ranking.contest != params.problem.contest or params.problem.problem != params.test_suite.problem:
-            raise CannotSetField()
-        params.save()
-        params.ranking.rejudge()
-        return params
+#    @ExportMethod(DjangoStruct('RankingParams'), [DjangoStruct('RankingParams')], PCArgField('fields', 'ranking', 'MANAGE'), [CannotSetField])
+#    @staticmethod
+#    def create(fields):
+#        params = RankingParams()
+#        params.forbid_fields(fields, ['id'])
+#        params.update_fields(fields, ['ranking', 'problem', 'test_suite'])
+#        if params.ranking.contest != params.problem.contest or params.problem.problem != params.test_suite.problem:
+#            raise CannotSetField()
+#        params.save()
+#        params.ranking.rejudge()
+#        return params
 
-    @ExportMethod(DjangoStruct('RankingParams'), [DjangoId('RankingParams'), DjangoStruct('RankingParams')], PCArg('self', 'MANAGE'), [CannotSetField])
-    def modify(self, fields):
-        self.forbid_fields(fields, ['id', 'ranking', 'problem'])
-        self.update_fields(fields, ['test_suite'])
-        if self.ranking.contest != self.problem.contest or self.problem.problem != self.test_suite.problem:
-            raise CannotSetField()
-        self.save()
-        self.ranking.rejudge()
-        return self
+#    @ExportMethod(DjangoStruct('RankingParams'), [DjangoId('RankingParams'), DjangoStruct('RankingParams')], PCArg('self', 'MANAGE'), [CannotSetField])
+#    def modify(self, fields):
+#        self.forbid_fields(fields, ['id', 'ranking', 'problem'])
+#        self.update_fields(fields, ['test_suite'])
+#        if self.ranking.contest != self.problem.contest or self.problem.problem != self.test_suite.problem:
+#            raise CannotSetField()
+#        self.save()
+#        self.ranking.rejudge()
+#        return self
 
 class RankingParamsEvents(Events):
     model = RankingParams
