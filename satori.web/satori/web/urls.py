5ee81f333c338bceff904500a68e4079a945aaf5
kalq28 <kalq28@gmail.com>
2011-02-14 17:08:28 +0100

    Moving satori.client.web to satori.web - main site + news.

--- satori.web/satori/web/urls.py
+++ satori.web/satori/web/urls.py
@@ -5,51 +5,57 @@ from django.conf import settings
 from satori.client.common import remote
 remote.setup(settings.THRIFT_HOST, settings.THRIFT_PORT, settings.BLOB_PORT)
 
+import os
+PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
+
 # Please keep the urlpatterns sorted.
 
 contestadminpatterns = patterns('',
-    (r'^$', 'satori.web.views.contest.admin.manage.load'),
-    (r'^contestants$', 'satori.web.views.contest.admin.contestants.load'),
-    (r'^news/(?P<id>\d+)$', 'satori.web.views.contest.admin.news.load'),
-    (r'^questions$', 'satori.web.views.contest.contest.admin.questions.load'),
-    (r'^questions/(?P<id>\d+)$', 'satori.web.views.contest.admin.editquestion.load'),
-    (r'^ranking/(?P<id>\d+)$', 'satori.web.views.contest.admin.ranking.load'),
-    (r'^subpage/(?P<id>\d+)$', 'satori.web.views.contest.admin.subpage.load'),
+    (r'^$', 'satori.web.views.contest.admin.manage.view'),
+    (r'^contestants$', 'satori.web.views.contest.admin.contestants.view'),
+    (r'^news/(?P<id>\d+)$', 'satori.web.views.contest.admin.news.view'),
+    (r'^questions$', 'satori.web.views.contest.contest.admin.questions.view'),
+    (r'^questions/(?P<id>\d+)$', 'satori.web.views.contest.admin.editquestion.view'),
+    (r'^ranking/(?P<id>\d+)$', 'satori.web.views.contest.admin.ranking.view'),
+    (r'^subpage/(?P<id>\d+)$', 'satori.web.views.contest.admin.subpage.view'),
 )
 
 contestpatterns = patterns('',
-    (r'^news$', 'satori.web.views.contest.news.load'),
-    (r'^print', 'satori.web.views.contest.print.load'),
-    (r'^problems$', 'satori.web.views.contest.problems.load'),
-    (r'^problems/(?P<id>\d+)$', 'satori.web.views.contest.editproblems.load'),
-    (r'^questions$', 'satori.web.views.contest.questions.load'),
-    (r'^ranking/(?P<id>\d+)$', 'satori.web.views.contest.ranking.load'),
-    (r'^results$', 'satori.web.views.contest.results.load'),
-    (r'^results/(?P<id>\d+)$', 'satori.web.views.contest.editresults.load'),
-    (r'^submit$', 'satori.web.views.contest.submit.load'),
-    (r'^subpage/(?P<id>\d+)$', 'satori.web.views.contest.subpage.load'),
+    (r'^news$', 'satori.web.views.contest.news.view'),
+    (r'^print', 'satori.web.views.contest.print.view'),
+    (r'^problems$', 'satori.web.views.contest.problems.view'),
+    (r'^problems/(?P<id>\d+)$', 'satori.web.views.contest.editproblems.view'),
+    (r'^questions$', 'satori.web.views.contest.questions.view'),
+    (r'^ranking/(?P<id>\d+)$', 'satori.web.views.contest.ranking.view'),
+    (r'^results$', 'satori.web.views.contest.results.view'),
+    (r'^results/(?P<id>\d+)$', 'satori.web.views.contest.editresults.view'),
+    (r'^submit$', 'satori.web.views.contest.submit.view'),
+    (r'^subpage/(?P<id>\d+)$', 'satori.web.views.contest.subpage.view'),
 
     (r'^admin/$', include(contestadminpatterns)),
 )
 
 adminpatterns = patterns('',
-    (r'^news/(?P<id>\d+)$', 'satori.web.views.admin.news.load'),
-    (r'^problems/(?P<id>\d+)$', 'satori.web.views.admin.problems.load'),
-    (r'^ranking/(?P<id>\d+)$', 'satori.web.views.admin.ranking.load'),
-    (r'^subpages/(?P<id>\d+)$', 'satori.web.views.admin.subpages.load'),
-    (r'^users$', 'satori.web.views.admin.users.load'),
-    (r'^users/(?P<id>\d+)$', 'satori.web.views.admin.edituser.load'),
+    (r'^news/(?P<id>\d+)$', 'satori.web.views.admin.news.view'),
+    (r'^problems/(?P<id>\d+)$', 'satori.web.views.admin.problems.view'),
+    (r'^ranking/(?P<id>\d+)$', 'satori.web.views.admin.ranking.view'),
+    (r'^subpages/(?P<id>\d+)$', 'satori.web.views.admin.subpages.view'),
+    (r'^users$', 'satori.web.views.admin.users.view'),
+    (r'^users/(?P<id>\d+)$', 'satori.web.views.admin.edituser.view'),
 )
 
 urlpatterns = patterns('',
-    (r'^$', 'satori.web.views.news.load'),
-    (r'^contests$', 'satori.web.views.contests.load'), 
-    (r'^login$', 'satori.web.views.login.load'), 
-    (r'^logout$', 'satori.web.views.logout.load'), 
-    (r'^news$', 'satori.web.views.news.load'), 
-    (r'^profile$', 'satori.web.views.profile.load'), 
-    (r'^subpage/(?P<id>\d+)$', 'satori.web.views.subpage.load'), 
+    (r'^$', 'satori.web.views.news.view'),
+#    (r'^contests$', 'satori.web.views.contests.view'), 
+#    (r'^login$', 'satori.web.views.login.view'), 
+#    (r'^logout$', 'satori.web.views.logout.view'), 
+    url(r'^news$', 'satori.web.views.news.view', name='news'), 
+#    (r'^profile$', 'satori.web.views.profile.view'), 
+#    (r'^subpage/(?P<id>\d+)$', 'satori.web.views.subpage.view'), 
 
-    (r'admin/$', include(adminpatterns)),
-    (r'contest/(?P<contestid>\d+)$', include(contestpatterns)),
+#    (r'admin/$', include(adminpatterns)),
+#    (r'contest/(?P<contestid>\d+)$', include(contestpatterns)),
+
+    (r'^files/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(PROJECT_PATH,'files')}),
 )
