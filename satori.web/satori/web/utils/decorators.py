4a167c837a6cd35600c9a4a332a9a8d09e702b2b
kalq28 <kalq28@gmail.com>
2011-02-14 19:41:38 +0100

    Fixed login in satori.web.

--- satori.web/satori/web/utils/decorators.py
+++ satori.web/satori/web/utils/decorators.py
@@ -1,28 +1,44 @@
 # vim:ts=4:sts=4:sw=4:expandtab
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from satori.client.common import want_import
 want_import(globals(), '*')
 
 def contest_view(func):
     def wrapped_contest_view(request, contestid, **kwargs):
+	    try:
+            token_container.set_token(request.COOKIES.get('satori_token', ''))
+        except:
+            token_container.set_token('')
         try:
             general_page_overview = Web.get_general_page_overview(Contest(contestid))
         except (TokenInvalid, TokenExpired):
-            #redirect to login page 
-            pass
+            token_container.set_token('')
+            res = HttpResponseRedirect(reverse('login'))
         except ArgumentNotFound:
             #show error page to 404
             pass
         else:
-            return func(request, general_page_overview, **kwargs)
+            res = func(request, general_page_overview, **kwargs)
+	    if request.COOKIES.get('satori_token', '') != token_container.get_token():
+	        res.set_cookie('satori_token', token_container.get_token())
+        return res
     return wrapped_contest_view
 
 def general_view(func):
     def wrapped_general_view(request, **kwargs):
+	    try:
+            token_container.set_token(request.COOKIES.get('satori_token', ''))
+        except:
+            token_container.set_token('')
         try:
             general_page_overview = Web.get_general_page_overview()
         except (TokenInvalid, TokenExpired):
-            #redirect to login page 
-            pass
+            token_container.set_token('')
+            res = HttpResponseRedirect(reverse('login'))
         else:
-            return func(request, general_page_overview, **kwargs)
+            res = func(request, general_page_overview, **kwargs)
+	    if request.COOKIES.get('satori_token', '') != token_container.get_token():
+	        res.set_cookie('satori_token', token_container.get_token())
+        return res
     return wrapped_general_view
