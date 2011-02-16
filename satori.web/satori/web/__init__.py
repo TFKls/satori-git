8d399e337a648883fcace92f75575a3f778cb8fd
marek j wrobel <marek.j.wrobel@gmail.com>
2011-02-16 16:50:48 +0100

    Added a hack for satori.web.manage.

--- satori.web/satori/web/__init__.py
+++ satori.web/satori/web/__init__.py
@@ -1,9 +1,24 @@
 # vim:ts=4:sts=4:sw=4:expandtab
 
+import os
 import satori.web.setup
 
 def manage():
     from django.core.management import execute_manager
     import satori.web.settings as settings
+
+    # HACK
+    import django.core.management
+
+    old_fmm = django.core.management.find_management_module
+
+    def find_management_module(app_name):
+        if app_name == 'satori.web':
+            return os.path.join(os.path.dirname(__file__), 'management')
+        else:
+            return old_fmm(app_name)
+
+    django.core.management.find_management_module = find_management_module
+    # END OF HACK
 
     execute_manager(settings)
