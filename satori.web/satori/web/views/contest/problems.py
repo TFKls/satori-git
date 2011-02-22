a3af6ef1889dc01d9577bfc02126da50108eecc0
duraj <duraj@tcs.uj.edu.pl>
2011-02-22 01:23:34 +0100

    Added ProblemMapping creation.

--- satori.web/satori/web/views/contest/problems.py
+++ satori.web/satori/web/views/contest/problems.py
@@ -2,22 +2,24 @@
 from satori.client.common import want_import
 want_import(globals(), '*')
 from satori.web.utils.decorators import contest_view
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django import forms
 from datetime import datetime
 from satori.web.utils.datetimewidget import SatoriSplitDateTime
 
 class ProblemsPublishForm(forms.Form):
-    submitstart = forms.DateTimeField(required=False,widget=SatoriSplitDateTime, initial=datetime.now())
-    submitfinish = forms.DateTimeField(required=False, widget=SatoriSplitDateTime, initial=datetime.now())
+    submitstart = forms.DateTimeField(required=False,widget=SatoriSplitDateTime)
+    submitfinish = forms.DateTimeField(required=False, widget=SatoriSplitDateTime)
+    viewauto = forms.BooleanField(required=False,initial=True)
+    viewstart = forms.DateTimeField(required=False,widget=SatoriSplitDateTime)
+    viewfinish = forms.DateTimeField(required=False, widget=SatoriSplitDateTime)
     def __init__(self,problem_list=[],data=None,*args,**kwargs):
         super(ProblemsPublishForm, self).__init__(data,*args, **kwargs)
         for pinfo in problem_list:
             if pinfo.is_admin:
                 self.fields[str(pinfo.problem_mapping.id)] = forms.BooleanField(required=False)
-    def clean(self):
-        data = self.cleaned_data
-        8/0
         
 
 
@@ -41,10 +43,35 @@ def view(request, page_info):
     problem_list = Web.get_problem_mapping_list(page_info.contest)
     if request.method=="POST":
         form = ProblemsPublishForm(data=request.POST,problem_list=problem_list)
-        valid = form.is_valid()
-        data = form.cleaned_data
-        7/0
-    form = ProblemsPublishForm(problem_list=problem_list)
+        if form.is_valid():
+            data = form.cleaned_data
+            selected = []
+            role = page_info.contest.contestant_role
+            for pinfo in problem_list:
+                id = str(pinfo.problem_mapping.id)
+                if id in request.POST.keys():
+                    selected.append(pinfo.problem_mapping)
+            if "changesubmit" in request.POST.keys():
+                for s in selected:
+                    start = data.get('submitstart',None)
+                    finish = data.get('submitfinish',None)
+                    Privilege.grant(role,s,'SUBMIT',PrivilegeTimes(start_on=start,finish_on=finish))
+                    if 'viewauto' in request.POST.keys():
+                        Privilege.grant(role,s,'VIEW',PrivilegeTimes(start_on=start))
+            elif "changeview" in request.POST.keys():
+                for s in selected:
+                    start = data.get('viewstart',None)
+                    finish = data.get('viewfinish',None)
+                    Privilege.grant(role,s,'VIEW',PrivilegeTimes(start_on=start,finish_on=finish))
+            elif "revokesubmit" in request.POST.keys():
+                for s in selected:
+                    Privilege.revoke(role,s,"SUBMIT")
+            elif "revokeview" in request.POST.keys():
+                for s in selected:
+                    Privilege.revoke(role,s,"VIEW")
+            problem_list = Web.get_problem_mapping_list(page_info.contest)
+    else:
+        form = ProblemsPublishForm(problem_list=problem_list)
     problems = []
     for pinfo in problem_list:
         p = {}
@@ -57,3 +84,39 @@ def view(request, page_info):
         p['when_submit'] = pinfo.contestant_role_submit_times
         problems.append(p)
     return render_to_response('problems.html', { 'page_info' : page_info, 'form' : form, 'problems' : problems})
+
+@contest_view
+def add(request, page_info):
+    if 'preid' in request.GET.keys():
+        preid = request.GET["preid"]
+        problem = Problem.filter(ProblemStruct(id=int(preid)))[0]
+        suites = TestSuite.filter(TestSuiteStruct(problem=problem))
+        suitechoices = [[suite.id,suite.name + '(' + suite.description + ')'] for suite in suites]
+        
+        class ProblemAddForm(forms.Form):
+            code = forms.CharField()
+            title = forms.CharField()
+            suite = forms.ChoiceField(choices=suitechoices)
+            statement = forms.CharField(widget=forms.Textarea, required=False)
+            
+        if request.method=="POST":
+            form = ProblemAddForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                suite = filter(lambda s:s.id==int(data["suite"]),suites)[0]
+                ProblemMapping.create(ProblemMappingStruct(contest=page_info.contest,problem=problem,code=data['code'],title=data['title'], statement=data['statement'],default_test_suite=suite))
+                return HttpResponseRedirect(reverse('contest_problems',args=[page_info.contest.id]))
+        else:
+            form = ProblemAddForm()
+            return render_to_response('problems_add.html', {'page_info' : page_info, 'form' : form, 'problem' : problem } )
+    else:
+        problems = Problem.filter()
+        choices = [[problem.id, problem.name + '(' + problem.description + ')'] for problem in problems]
+
+        class ProblemPreAddForm(forms.Form):
+            preid = forms.ChoiceField(choices=choices,label='Use problem')
+        
+        form = ProblemPreAddForm()
+        return render_to_response('problems_add.html', { 'page_info' : page_info, 'form' : form } )
+        
+        
