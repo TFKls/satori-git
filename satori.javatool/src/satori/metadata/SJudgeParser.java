2b5c2d81f6d96c0fd196dec97dfe1873b323ef39
bartosz m walczak <bartosz.m.walczak@gmail.com>
2011-02-27 19:16:37 +0100

    parsing output metadata

--- satori.javatool/src/satori/metadata/SJudgeParser.java
+++ satori.javatool/src/satori/metadata/SJudgeParser.java
@@ -64,49 +64,25 @@ public class SJudgeParser {
 		if (!required.equals("true") && !required.equals("false")) throw new ParseException("Invalid input required mode: " + required); 
 		String def_value = node.getAttribute("default");
 		if (def_value.isEmpty()) def_value = null;
-		return new SInputMetadata(name, desc, type, required.equals("true"), def_value); 
+		if (def_value != null && type == SBlobType.INSTANCE) throw new ParseException("Default value for blob inputs not supported");
+		return new SInputMetadata(name, desc, type, required.equals("true"), def_value);
 	}
 	
-	/*private static OutputMetadata parseOutput(Element node) throws ParseException {
+	private static SOutputMetadata parseOutputParam(Element node) throws ParseException {
+		String type_str = node.getAttribute("type");
+		if (type_str.isEmpty()) throw new ParseException("Output type undefined");
+		SType type;
+		if (type_str.equals("text")) type = STextType.INSTANCE;
+		else if (type_str.equals("time")) type = STimeType.INSTANCE;
+		else if (type_str.equals("size")) type = SSizeType.INSTANCE;
+		else if (type_str.equals("blob")) type = SBlobType.INSTANCE;
+		else throw new ParseException("Unsupported output type: " + type_str);
 		String name = node.getAttribute("name");
 		if (name.isEmpty()) throw new ParseException("Output name undefined");
 		String desc = node.getAttribute("description");
 		if (desc.isEmpty()) throw new ParseException("Output description undefined");
-		String ret_mode = node.getAttribute("return");
-		if (ret_mode.isEmpty()) throw new ParseException("Output return mode undefined");
-		boolean ret_on_success = ret_mode.equals("always") || ret_mode.equals("success");
-		boolean ret_on_failure = ret_mode.equals("always") || ret_mode.equals("failure");
-		if (!ret_on_success && !ret_on_failure) throw new ParseException("Invalid output return mode: " + ret_mode);
-		String requested = node.getAttribute("default_requested");
-		if (requested.isEmpty()) throw new ParseException("Output default requested mode undefined");
-		if (!requested.equalsIgnoreCase("true") && !requested.equalsIgnoreCase("false")) throw new ParseException("Invalid output default requested mode: " + requested); 
-		String type = node.getAttribute("type");
-		if (type.isEmpty()) throw new ParseException("Input type undefined");
-		OutputMetadata meta;
-		if (type.equals("value")) meta = new ValueOutputMetadata(name, desc, ret_on_success, ret_on_failure, requested.equals("true"));  
-		else if (type.equals("file")) throw new ParseException("Output files not supported yet");
-		else throw new ParseException("Invalid input type: " + type);
-		return meta; 
-	}*/
-	
-	/*private static StageMetadata parseStage(Element node) throws ParseException {
-		String name = node.getAttribute("name");
-		if (name.isEmpty()) throw new ParseException("Stage name undefined");
-		String desc = node.getAttribute("description");
-		if (desc.isEmpty()) throw new ParseException("Stage description undefined");
-		String enabled = node.getAttribute("default_enabled");
-		if (enabled.isEmpty()) throw new ParseException("Stage default enabled mode undefined");
-		if (!enabled.equals("true") && !enabled.equals("false")) throw new ParseException("Invalid stage default enabled mode: " + enabled); 
-		StageMetadata stage = new StageMetadata(name, desc, enabled.equals("true"));
-		NodeList children = node.getElementsByTagName("*");
-		for (int i = 0; i < children.getLength(); ++i) {
-			Element child = (Element)children.item(i);
-			if (child.getNodeName().equals("input")) stage.addInput(parseInput(child));
-			else if (child.getNodeName().equals("output")) stage.addOutput(parseOutput(child));
-			else throw new ParseException("Incorrect node: " + child.getNodeName());
-		}
-		return stage;
-	}*/
+		return new SOutputMetadata(name, desc, type);
+	}
 	
 	private static List<SInputMetadata> parseInputs(Element node) throws ParseException {
 		List<SInputMetadata> result = new ArrayList<SInputMetadata>();
@@ -115,13 +91,27 @@ public class SJudgeParser {
 		return Collections.unmodifiableList(result);
 	}
 	
+	private static List<SOutputMetadata> parseOutputs(Element node) throws ParseException {
+		List<SOutputMetadata> result = new ArrayList<SOutputMetadata>();
+		NodeList children = node.getElementsByTagName("param");
+		for (int i = 0; i < children.getLength(); ++i) result.add(parseOutputParam((Element)children.item(i)));
+		return Collections.unmodifiableList(result);
+	}
+	
 	private static Result parse(Document doc) throws ParseException {
 		doc.normalizeDocument();
 		Element node = doc.getDocumentElement();
 		NodeList input_children = node.getElementsByTagName("input");
-		if (input_children.getLength() != 1) throw new ParseException("Incorrect number of input groups");
-		List<SInputMetadata> input_meta = parseInputs((Element)input_children.item(0));
-		return new Result(input_meta, null);
+		List<SInputMetadata> input_meta;
+		if (input_children.getLength() == 0) input_meta = Collections.emptyList();
+		else if (input_children.getLength() == 1) input_meta = parseInputs((Element)input_children.item(0));
+		else throw new ParseException("Too many input groups");
+		NodeList output_children = node.getElementsByTagName("input");
+		List<SOutputMetadata> output_meta;
+		if (output_children.getLength() == 0) output_meta = Collections.emptyList();
+		else if (output_children.getLength() == 1) output_meta = parseOutputs((Element)output_children.item(0));
+		else throw new ParseException("Too many output groups");
+		return new Result(input_meta, output_meta);
 	}
 	private static Result parse(String str) throws ParseException {
 		InputSource is = new InputSource();
