1c6b74513544c4f9cb92c3a71d697bfd806a0f0a
kalq28 <kalq28@gmail.com>
2011-03-04 13:41:44 +0100

    Added render_to_json shortcut.

--- satori.web/satori/web/utils/shortcuts.py
+++ satori.web/satori/web/utils/shortcuts.py
@@ -1,8 +1,16 @@
 import os
+from django.shortcuts import render_to_response
 from satori.web.sphinx.translator import rendertask
 from satori.web.urls import PROJECT_PATH
 
+def render_to_json(*args, **kwargs):
+    response = render_to_response(*args, **kwargs)
+    response['mimetype'] = "text/javascript"
+    response['Pragma'] = "no cache"
+    response['Cache-Control'] = "no-cache, must-revalidate"
+
+    return response
+
 def text2html(text):
     return rendertask(unicode(text), os.path.join(PROJECT_PATH, 'files/tmp'), 'files/tmp')
-    
 
