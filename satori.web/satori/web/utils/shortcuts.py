ce48664b36fc83bd555b8adbe82d35f56700262f
marek j wrobel <marek.j.wrobel@gmail.com>
2011-03-04 13:38:59 +0100

    Improved rest handling.

--- satori.web/satori/web/utils/shortcuts.py
+++ satori.web/satori/web/utils/shortcuts.py
@@ -1,8 +1,14 @@
 import os
 from satori.web.sphinx.translator import rendertask
 from satori.web.urls import PROJECT_PATH
+from django.core.urlresolvers import reverse
+import re
 
 def text2html(text):
     return rendertask(unicode(text), os.path.join(PROJECT_PATH, 'files/tmp'), 'files/tmp')
     
+def fill_image_links(content, model, id, group):
+    def imagerepl(matchobj):
+        return '"' + reverse('download_group', args=['view', model, id, group, matchobj.group(1), matchobj.group(1)]) + '"'
+    return re.sub(r'"_images/([^"]+)"', imagerepl, content)
 
