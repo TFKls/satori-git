dcdc256c2c63cb45a7e443868e30ab872a29a557
mwrobel <devnull@localhost>
2011-11-03 23:26:19 +0100

    Fixed setup scripts to include data files in distribution.

--- satori.web/setup.py
+++ satori.web/setup.py
@@ -1,5 +1,20 @@
 # vim:ts=4:sts=4:sw=4:expandtab
 from setuptools import setup, find_packages
+from distutils.util import convert_path
+import os
+
+def find_files(where='.', prefix=''):
+    out = []
+    stack=[(convert_path(where), prefix)]
+    while stack:
+        where,prefix = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where,name)
+            if name != '..' and name != '.' and os.path.isdir(fn):
+                stack.append((fn,prefix+name+'/'))
+            elif os.path.isfile(fn):
+                out.append(prefix+name); 
+    return out
 
 setup(name='satori.web',
     packages=find_packages(),
@@ -18,4 +33,7 @@ setup(name='satori.web',
         [console_scripts]
 	    satori.web.manage = satori.web:manage
     ''',
+    package_data={
+        'satori.web': find_files('satori/web/templates', 'templates/') + find_files('satori/web/files', 'files/'),
+    },
 )
