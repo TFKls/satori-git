fb698b5b2dbda783d3b711a4be5421c95eb4415a
paladin <duraj@tcs.uj.edu.pl>
2011-12-05 15:28:13 +0100

    ACMProblemStats: another typo.

--- satori.core/satori/core/checking/aggregators.py
+++ satori.core/satori/core/checking/aggregators.py
@@ -526,8 +526,9 @@ class ACMProblemStats(AggregatorBase):
         self.ranking.header = self.table.row_separator + self.table.header_row + self.table.header_separator
         for p in self.problem_list:
             col = [p.code,unicode(self.total[p])] + [unicode(self.stats[p][r]) for r in self.results]
-            self.ranking.header += self.table.generate_row(*col)               
-        self.ranking.header += self.table.row_separator
+            self.ranking.header += self.table.generate_row(*col)+self.table.row_separator               
+        if not self.problem_list:
+            self.ranking.header += self.table.row_separator
         self.ranking.save()
     
     def init(self):
