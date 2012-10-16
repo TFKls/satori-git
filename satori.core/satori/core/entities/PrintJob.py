274cb41e95f3b736fa531d395df9dd1bc672d20f
mammoth <devnull@localhost>
2012-10-16 23:32:33 +0200

    printing implementation enhanced.

--- satori.core/satori/core/entities/PrintJob.py
+++ satori.core/satori/core/entities/PrintJob.py
@@ -14,11 +14,14 @@ class PrintJob(Entity):
     contestant    = models.ForeignKey('Contestant', related_name='printjobs', on_delete=models.CASCADE)
     contest       = models.ForeignKey('Contest', related_name='printjobs', on_delete=models.CASCADE)
     time          = models.DateTimeField(auto_now_add=True)
+    pending       = models.BooleanField(default=True)
+    status        = models.CharField(max_length=64)
+    report        = models.TextField()
 
     data          = AttributeGroupField(PCArg('self', 'VIEW'), PCDeny(), '')
 
     class ExportMeta(object):
-        fields = [('contestant', 'VIEW'), ('contest', 'VIEW'), ('time', 'VIEW')]
+        fields = [('contestant', 'VIEW'), ('contest', 'VIEW'), ('time', 'VIEW'), ('pending', 'VIEW'), ('status', 'VIEW'), ('report', 'VIEW')]
 
     class RightsMeta(object):
         inherit_parent = 'contestant'
@@ -34,8 +37,9 @@ class PrintJob(Entity):
     def create(fields, content, filename):
         printjob = PrintJob()
         printjob.contestant = fields.contest.find_contestant(token_container.token.role)
-        printjob.forbid_fields(fields, ['id', 'contestant', 'time'])
+        printjob.forbid_fields(fields, ['id', 'contestant', 'time', 'pending', 'status', 'report'])
         printjob.update_fields(fields, ['contest'])
+        printjob.status = 'QUE'
         printjob.save()
         blob = printjob.data_set_blob('content', filename=filename)
         blob.write(content)
@@ -52,6 +56,10 @@ class PrintJob(Entity):
 
     @ExportMethod(NoneType, [DjangoId('PrintJob')], PCArg('self', 'MANAGE'))
     def reprint(self):
+        self.pending = True
+        self.status = 'QUE'
+        self.report = None
+        self.save()
         RawEvent().send(Event(type='printjob', id=self.id))
 
 class PrintJobEvents(Events):
--- satori.core/satori/core/printing/__init__.py
+++ satori.core/satori/core/printing/__init__.py
@@ -11,15 +11,24 @@ import tempfile
 from satori.core.models import *
 from satori.events import Event, Client2
 
-serial = 1
+#TODO: Launch no more than n jobs simultaneously
 
-def printer_func(script, path, filename):
+def printer_func(script, path, filename, printjob):
     tmp = None
     try:
         tmp = tempfile.mkdtemp()
         os.chdir(tmp)
         os.symlink(path, filename)
-        subprocess.call([script, filename])
+        sub = subprocess.Popen([script, filename, unicode(printjob.id)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = sub.communicate()
+        if sub.returncode == 0:
+            printjob.status = 'OK'
+            printjob.report = out
+        else:
+            printjob.status = 'FAIL'
+            printjob.report = out + err
+        printjob.pending = False
+        printjob.save()
     finally:
         if(tmp is not None):
             shutil.rmtree(tmp)
@@ -34,6 +43,8 @@ class PrintingMaster(Client2):
     def init(self):
         self.attach(self.queue)
         self.map({'type': 'printjob'}, self.queue)
+        for printjob in PrintJob.objects.filter(pending=True):
+            self.printjob_queue.append(printjob)
         self.do_work()
 
     def do_work(self):
@@ -53,6 +64,8 @@ class PrintingMaster(Client2):
         if (printer is not None):
             oa = printjob.data_get('content');
             path = os.path.join(settings.BLOB_DIR, oa.value[0], oa.value[1], oa.value[2], oa.value)
-            p=Process(target=printer_func, args=(printer.script, path, oa.filename))
-            p.daemon=True
+            p = Process(target=printer_func, args=(printer.script, path, oa.filename, printjob))
+            p.daemon = True
+            printjob.status = 'PRN'
+            printjob.save()
             p.start()
