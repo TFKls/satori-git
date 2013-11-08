645bf89eb3bc70068541ebfc2f45f82573fae1bd
Robert Obryk <robryk@gmail.com>
2013-11-08 14:34:54 +0100

    satori.core.management: distinguish log output from different processes
    Tested: "satori.core start" up to DB init, prints expected log messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ pushd "$OFFICE"
 apt-get -y install python-virtualenv python-dev libpopt-dev libcurl4-openssl-dev libpq-dev libyaml-dev libcap-dev make patch
 unset PYTHONPATH
 
-virtualenv --no-site-packages --prompt=\(satori\) "$VENV" &&
+virtualenv2 --no-site-packages --prompt=\(satori\) "$VENV" &&
 mkdir -p bin &&
 find "$VENV/bin" -name "activate*" |while read a; do ln -s "../$a" bin; done &&
 source bin/activate &&
