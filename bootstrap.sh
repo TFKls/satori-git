c954a39c08a1f0a280f926733d5279f597c61438
Robert Obryk <robryk@gmail.com>
2013-11-08 14:40:14 +0100

    bootstrap: use virtualenv2 instead of virtualenv if available
    On Arch Linux all non-suffixed python binaries correspond to python3
    packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,12 @@ pushd "$OFFICE"
 apt-get -y install python-virtualenv python-dev libpopt-dev libcurl4-openssl-dev libpq-dev libyaml-dev libcap-dev make patch
 unset PYTHONPATH
 
-virtualenv2 --no-site-packages --prompt=\(satori\) "$VENV" &&
+VENV_BIN=virtualenv2
+if ! which "$VENV_BIN" >& /dev/null; then
+	VENV_BIN=virtualenv
+fi
+
+"$VENV_BIN" --no-site-packages --prompt=\(satori\) "$VENV" &&
 mkdir -p bin &&
 find "$VENV/bin" -name "activate*" |while read a; do ln -s "../$a" bin; done &&
 source bin/activate &&
