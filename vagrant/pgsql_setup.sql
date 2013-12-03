1f0e8ca3810fb5c6ba5b9da2bb6e1377633ee4b9
Grzegorz Gutowski <grzegorz.gutowski@tcs.uj.edu.pl>
2013-12-03 21:51:13 +0100

    Repair vagrant: core postgres database init script.

--- vagrant/pgsql_setup.sql
+++ vagrant/pgsql_setup.sql
@@ -1,5 +1,5 @@
 create user vagrant with createdb password '';
 create database vagrant with owner vagrant;
 grant all on database vagrant to vagrant;
-\c template1
+\c vagrant
 create extension tablefunc;
