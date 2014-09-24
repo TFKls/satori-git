67183fb9560e18bb05f1d0e853edd22ad77144cd
Grzegorz Gutowski <grzegorz.gutowski@tcs.uj.edu.pl>
2014-09-25 01:02:31 +0200

    Don't parse testd arguments in inside mode of satori_test

--- satori-testing/test/run.sh
+++ satori-testing/test/run.sh
@@ -2,5 +2,5 @@
 OFFICE=$(dirname "$(readlink -f "$(which "$0")")")
 pushd "$OFFICE"
 rm -rf result
-satori_test --cpus 2-3 --time 300 --memory $[4*1024*1024*1024] howto_skip_this_hack_in_argparse --test test.yaml --submit test/sol.cpp --result result
+../satori_test --cpus 2-3 --time 300 --memory $[4*1024*1024*1024] testd --test test.yaml --submit test/sol.cpp --result result
 popd
